5563a4bccac1248555ffd341dcb010a165a397ac
Fardeen Khan <fk7703890@gmail.com>
2026-06-13 10:14:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import streamlit as st
 import pickle
 import re
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import plotly.io as pio
 
 # ==========================================================
 # NLTK
